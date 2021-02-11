9ff904fcc1c4183a4fb77446639fb690f5f67b6b
luCAOrx <lucaorx@protonmail.com>
2021-02-11 03:47:10 -0300

    Adicionado locale

---
+++ mobile/src/config/locale.ts
@@ -0,0 +1,14 @@
+import { LocaleConfig } from 'react-native-calendars';
+
+const locale = () => {
+  LocaleConfig.locales["br"] = {
+    monthNames: ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"],
+    monthNamesShort: ["Jan","Fev","Mar","Abr","Mai","Jun","Jul","Ago","Set","Out","Nov","Dez"],
+    dayNames: ["Segunda-feira","Terça-feira","Quarta-feira","Quinta-feira","Sexta-feira","Sábado","Domingo"],
+    dayNamesShort: ["Do","Se","Te","Qu","Qu","Se","Sá"],
+  }
+  
+  LocaleConfig.defaultLocale = "br";
+}
+
+export default locale;
