92f31e80dbabea70acba12691ff03fe9df9a490c
luCAOrx <lucaorx@protonmail.com>
2021-03-24 21:46:38 -0300

    Retirado import desnecessário

--- mobile/src/components/Button/index.tsx
+++ mobile/src/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { StyleSheet, Text } from 'react-native';
-import { RectButton, RectButtonProperties, TouchableOpacity } from 'react-native-gesture-handler';
+import { RectButtonProperties, TouchableOpacity } from 'react-native-gesture-handler';
 
 interface CustomButtonProps extends RectButtonProperties {
   title: string;
