fb3363df500d6469b7a2e38fc605574d835e018a
luCAOrx <lucaorx@protonmail.com>
2021-08-03 02:22:43 -0300

    Add erro global para página não encontrada

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,6 +16,11 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.use((request, response, next) => {
+  next(response.status(404).json({ erro: 'Página não encontrada' }));
+});
+
 app.use(errorHandler);
 
 export default app;
