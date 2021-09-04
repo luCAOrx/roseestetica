18b7a07cbd3d11d087a8523dd6b41f4ad7efa9cf
luCAOrx <lucaorx@protonmail.com>
2021-09-04 04:53:09 -0300

    Transformado export default em const

--- mobile/src/themes/dark.ts
+++ mobile/src/themes/dark.ts
@@ -1,4 +1,6 @@
-export default {
+import { CustomTheme } from '@react-navigation/native';
+
+export const dark: CustomTheme = {
   dark: true,
   colors: {
     primary: '#7A7A7A',
@@ -15,18 +17,19 @@ export default {
     separator: 'rgba(0,0,0,0.3)',
     border: '#5C5C5C',
 
-    currentStepLabelColor: "#333333",
+    currentStepLabelColor: '#333333',
   
-    stepStrokeCurrentColor: "#2FB86E",
+    stepStrokeCurrentColor: '#2FB86E',
   
-    stepIndicatorLabelCurrentColor: "#333333",
-    stepIndicatorCurrentColor: "#D2D2E3",
-    stepIndicatorFinishedColor: "#2FB86E",
-    stepIndicatorUnFinishedColor: "#D2D2E3",
-    stepIndicatorLabelFinishedColor: "#333333",
-    stepIndicatorLabelUnFinishedColor: "#333333",
+    stepIndicatorLabelCurrentColor: '#333333',
+    stepIndicatorCurrentColor: '#D2D2E3',
+    stepIndicatorFinishedColor: '#2FB86E',
+    stepIndicatorUnFinishedColor: '#D2D2E3',
+    stepIndicatorLabelFinishedColor: '#333333',
+    stepIndicatorLabelUnFinishedColor: '#333333',
   
-    separatorFinishedColor: "#2FB86E",
-    separatorUnFinishedColor: "#D2D2E3"
+    separatorFinishedColor: '#2FB86E',
+    separatorUnFinishedColor: '#D2D2E3',
+    notification: '#D2D2E3'
   },
-};
+}
