abcad961e1786211fe86349f7a0b022b1227c42d
luCAOrx <lucaorx@protonmail.com>
2021-09-17 01:11:02 -0300

    Adicionado mais uma verificação de propriedades

--- mobile/__tests__/components/SucessScreen/sucessScreen.test.tsx
+++ mobile/__tests__/components/SucessScreen/sucessScreen.test.tsx
@@ -9,13 +9,14 @@ import styles from '../../../src/components/SucessScreen/styles';
 
 describe('The sucess screen', () => {
   it('must have the same style and properties defined in the component.', () => {
-    const { getByTestId } = render(<SucessScreen title="Teste" show/>);
-
     const sucessScreen = render(<SucessScreen title="Teste" show/>);
 
-    expect(sucessScreen.container.props.show).toBeTruthy();
-    expect(getByTestId('sucessScreenContainer')).toHaveStyle(styles.container);
-    expect(getByTestId('sucessScreenTitle')).toHaveStyle(styles.text);
-    expect(getByTestId('sucessScreenTitle')).toHaveTextContent('Teste');
+    expect(sucessScreen.container.findByProps({show: true})).toHaveProp('show', true);
+
+    expect(sucessScreen.getByTestId('sucessScreenContainer')).toHaveStyle(styles.container);
+
+    expect(sucessScreen.getByTestId('sucessScreenTitle')).toHaveStyle(styles.text);
+
+    expect(sucessScreen.getByTestId('sucessScreenTitle')).toHaveTextContent('Teste');
   });
 });
