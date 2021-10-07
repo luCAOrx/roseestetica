507844d6c8d7ca13958e83ae2fbbf56b98080342
luCAOrx <lucaorx@protonmail.com>
2021-10-07 19:50:03 -0300

    Atualizado variáveis ambiente

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,7 +1,7 @@
 import knex from 'knex';
 const configuration = require('../../knexfile');
 
-const config = process.env.NODE_ENV === 'production' ? configuration.production : configuration.development;
+const config = process.env.DATABASE_ENV === 'production' ? configuration.production : configuration.development;
 
 const connection = knex(config);
 
