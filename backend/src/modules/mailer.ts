975d71c151ae23b3f5240270bb9ca66051c1ab5e
luCAOrx <lucaorx@protonmail.com>
2021-10-07 19:38:56 -0300

    Atualizado transport

--- backend/src/modules/mailer.ts
+++ backend/src/modules/mailer.ts
@@ -5,14 +5,18 @@ import path from 'path';
 
 import mailConfig from '../config/mail';
 
-const { host, port, user, pass } = mailConfig;
+const { host, port, secure, user, pass, ciphers } = mailConfig;
 
 const transport = nodemailer.createTransport({
   host,
   port,
+  secure,
   auth: {
     user,
     pass
+  },
+  tls: {
+    ciphers
   }
 });
 
