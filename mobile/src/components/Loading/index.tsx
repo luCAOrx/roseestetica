e2b78de2880e0db136516fb7a4ca8909e17b3a60
luCAOrx <lucaorx@protonmail.com>
2021-10-13 00:47:55 -0300

    Alterado a cor

--- mobile/src/components/Loading/index.tsx
+++ mobile/src/components/Loading/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+
 import { ActivityIndicator } from 'react-native';
 
+import { useTheme } from '@react-navigation/native';
+
 export default function Loading() {
+  const { colors } = useTheme();
+
   return (
     <ActivityIndicator 
       size="large"
-      color="#333"
+      color={colors.text}
       animating
       style={{ justifyContent: 'center', alignItems: 'center' }}
       testID="loading"
