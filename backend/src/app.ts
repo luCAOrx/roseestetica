f06c66d9cd7d7f33354769a94049645a63106f45
luCAOrx <lucaorx@protonmail.com>
2022-08-11 01:20:23 -0300

    Removido middleware de urlencoded

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
