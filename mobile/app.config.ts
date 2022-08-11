743dda5b60daa4b5446460aaa69dc244c5fff543
luCAOrx <lucaorx@protonmail.com>
2022-08-11 08:06:19 -0300

    Adicionado arquivo de configuração do expo com exntensão para typescript

---
+++ mobile/app.config.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config'
+
+export default {
+  name: "Rose Estética",
+  slug: "roseestetica",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "cover"
+  },
+  androidNavigationBar: {
+    backgroundColor: "#000000",
+    barStyle: "light-content"
+  },
+  androidStatusBar: {
+    backgroundColor: "#000000",
+    barStyle: "light-content"
+  },
+  updates: {
+    fallbackToCacheTimeout: 0
+  },
+  assetBundlePatterns: [
+    "**/*"
+  ],
+  ios: {
+    supportsTablet: true
+  },
+  web: {
+    favicon: "./assets/icon.png"
+  },
+  extra: {
+    serverUrl: process.env.SERVER_URL
+  }
+}
