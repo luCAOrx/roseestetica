6787efd83118c0343868b93886f4264d6c442a66
luCAOrx <lucaorx@protonmail.com>
2022-08-11 08:32:03 -0300

    Finalizado atualização das novas versões das bibliotecas

--- backend/README.md
+++
@@ -1,4 +0,0 @@
-<h1 align="center">
-    <img alt="RoseEstetica" src=".github/logo.png" width="350px" />
-</h1>
-<h3>Aplicação em desenvolvimento ...</h3>
--- backend/src/database/connection.ts
+++
@@ -1,8 +0,0 @@
-import knex from 'knex';
-const configuration = require('../../knexfile');
-
-const config = process.env.DATABASE_ENV === 'production' ? configuration.production : configuration.development;
-
-const connection = knex(config);
-
-export default connection;
