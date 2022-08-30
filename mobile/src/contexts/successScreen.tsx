8212154e6fc6ad840d8b2a45cc4cc3b68a50a5ac
luCAOrx <lucaorx@protonmail.com>
2022-08-30 06:08:47 -0300

    Adicionado contexto e hook de tela de sucesso

---
+++ mobile/src/contexts/successScreen.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useState } from 'react'
+import SucessScreen from '../components/SucessScreen';
+
+interface SuccessScreenProps {
+  handleShowSuccessMessage: (showSuccessMessage: boolean) => void;
+  handleTitleSuccessMessage: (titleSuccessMessage: string) => void;
+}
+
+const SuccessScreenContext = createContext<SuccessScreenProps>(
+  {} as SuccessScreenProps
+);
+
+export const SuccessScreenProvider: React.FC = ({ children }) => {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [titleSuccessMessage, setTitleSuccessMessage] = useState('');
+
+  function handleShowSuccessMessage(showSuccessMessage: boolean) {
+    setShowSuccessMessage(showSuccessMessage)
+  }
+
+  function handleTitleSuccessMessage(titleSuccessMessage: string) {
+    setTitleSuccessMessage(titleSuccessMessage)
+  }
+
+  return (
+    <SuccessScreenContext.Provider
+      value={{ handleShowSuccessMessage, handleTitleSuccessMessage }}
+    >
+      {children}
+      <SucessScreen title={titleSuccessMessage} show={showSuccessMessage}/>
+    </SuccessScreenContext.Provider>
+  );
+};
+
+export function useSuccessScreen() {
+  const context = useContext(SuccessScreenContext);
+
+  return context;
+};
