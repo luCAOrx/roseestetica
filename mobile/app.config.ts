ca4f9267a66a4757344b60887d4ee09b95ca87ae
luCAOrx <lucaorx@protonmail.com>
2022-08-31 00:41:39 -0300

    Identado linhas

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -1,34 +1,34 @@
 import 'dotenv/config'
 
 export default {
-  name: "Rose Estética",
-  slug: "roseestetica",
-  version: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/icon.png",
+  name: 'Rose Estética',
+  slug: 'roseestetica',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
   splash: {
-    image: "./assets/splash.png",
-    resizeMode: "cover"
+    image: './assets/splash.png',
+    resizeMode: 'cover'
   },
   androidNavigationBar: {
-    backgroundColor: "#000000",
-    barStyle: "light-content"
+    backgroundColor: '#000000',
+    barStyle: 'light-content'
   },
   androidStatusBar: {
-    backgroundColor: "#000000",
-    barStyle: "light-content"
+    backgroundColor: '#000000',
+    barStyle: 'light-content'
   },
   updates: {
     fallbackToCacheTimeout: 0
   },
   assetBundlePatterns: [
-    "**/*"
+    '**/*'
   ],
   ios: {
     supportsTablet: true
   },
   web: {
-    favicon: "./assets/icon.png"
+    favicon: './assets/icon.png'
   },
   extra: {
     serverUrl: process.env.SERVER_URL
