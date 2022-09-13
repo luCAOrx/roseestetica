38aeb805cc3bccf20a9642555633dc2d69394e49
luCAOrx <lucaorx@protonmail.com>
2022-09-13 02:31:13 -0300

    Removido validação para os espaços em branco

--- mobile/src/pages/Dashboard/Profile/ChangeAddress.tsx
+++ mobile/src/pages/Dashboard/Profile/ChangeAddress.tsx
@@ -57,7 +57,7 @@ export default function ChangeAddress() {
 
     const regexLetras = /^([a-zA-Zà-úÀ-Ú]|\s+)+$/
 
-    const {
+    let {
       cidade_id,
       bairro,
       logradouro,
@@ -78,29 +78,25 @@ export default function ChangeAddress() {
     try {
       const schema = Yup.object().shape({
         cidade_id: Yup.number().required('O campo cidade é obrigatório!'),
-        bairro: Yup.string().strict(true)
-          .trim('Não são permitidos espaços no começo ou no fim!')
+        bairro: Yup.string()
           .matches(regexLetras, 'O campo bairro só aceita letras!')
           .min(3, 'No mínimo 3 caracteres!')
           .max(90, 'No máximo 90 caracteres!')
           .required('O campo bairro é obrigatório!'),
-        logradouro: Yup.string().strict(true)
-          .trim('Não são permitidos espaços no começo ou no fim!')
+        logradouro: Yup.string()
           .matches(regexLetras, 'O campo logradouro só aceita letras!')
           .min(5, 'No mínimo 5 caracteres!')
           .max(90, 'No máximo 90 caracteres!')
           .required('O campo logradouro é obrigatório!'),
-        numero: Yup.string().strict(true)
-          .trim('Não são permitidos espaços no começo ou no fim!')
+        numero: Yup.string()
           .min(1, 'No mínimo 1 caractere!')
           .max(6, 'No máximo 6 caracteres!')
           .required('O campo número é obrigatório!'),
-        complemento: Yup.string().optional().strict(true)
+        complemento: Yup.string().optional()
           .matches(/.{5,}/, {
             excludeEmptyString: true,
             message: 'No mínimo 5 caracteres!'
           })
-          .trim('Não são permitidos espaços no começo ou no fim!')
           .max(90, 'No máximo 90 caracteres!'),
         cep: Yup.string()
           .min(8, 'No mínimo 8 caracteres!')
@@ -113,6 +109,10 @@ export default function ChangeAddress() {
       })
 
       formRef.current?.setErrors({})
+
+      bairro = bairro.trim()
+      logradouro = logradouro.trim()
+      complemento = complemento.trim()
 
       setIsRequested(true)
 
